ceb79dc84bb9be73d2f1e088cd1e0692b02d7f10
al2wyw <yl0863758497@gmail.com>
2014-04-21 14:14:55 +0800

    first commit

---
+++ test_ignore/src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+}
