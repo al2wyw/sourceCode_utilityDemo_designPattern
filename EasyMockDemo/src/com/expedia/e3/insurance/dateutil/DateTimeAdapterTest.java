1c7eaef23d6b3edc40b579750551d877421d2cf3
P0043626 <P0043626@LP0043626.sz.vsg.com>
2014-07-23 12:24:01 +0800

    init easymock with libs

--- EasyMockDemo/src/com/expedia/e3/insurance/dateutil/DateTimeAdapterTest.java
+++ EasyMockDemo/src/com/expedia/e3/insurance/dateutil/DateTimeAdapterTest.java
@@ -1,14 +1,18 @@
 package com.expedia.e3.insurance.dateutil;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class DateTimeAdapterTest {
-	
+
 	@Test
-	public void testParseDate(){
+	public void testParseDate() {
 		DateTime dt = DateTimeAdapter.parseDate("2013-05-12");
 		Assert.assertNotNull(dt);
 		Assert.assertEquals(2013, dt.getYear());
@@ -19,9 +23,9 @@ public class DateTimeAdapterTest {
 		Assert.assertEquals(0, dt.getSecondOfMinute());
 		Assert.assertEquals(0, dt.getMillisOfSecond());
 	}
-	
+
 	@Test
-	public void testParseDateTime(){ //no exception!!
+	public void testParseDateTime() { // no exception!!
 		DateTime dt = DateTimeAdapter.parseDateTime("2013-05-12T23:11:20");
 		Assert.assertNotNull(dt);
 		Assert.assertEquals(2013, dt.getYear());
@@ -32,10 +36,11 @@ public class DateTimeAdapterTest {
 		Assert.assertEquals(20, dt.getSecondOfMinute());
 		Assert.assertEquals(0, dt.getMillisOfSecond());
 	}
-	
+
 	@Test
-	public void testParseDBDateTimeOffset(){
-		DateTime dt = DateTimeAdapter.parseDBDateTimeOffset("2013-05-12 10:42:11 +08:00");
+	public void testParseDBDateTimeOffset() {
+		DateTime dt = DateTimeAdapter
+				.parseDBDateTimeOffset("2013-05-12 10:42:11 +08:00");
 		Assert.assertNotNull(dt);
 		Assert.assertEquals(2013, dt.getYear());
 		Assert.assertEquals(5, dt.getMonthOfYear());
@@ -44,32 +49,65 @@ public class DateTimeAdapterTest {
 		Assert.assertEquals(42, dt.getMinuteOfHour());
 		Assert.assertEquals(11, dt.getSecondOfMinute());
 		Assert.assertEquals(0, dt.getMillisOfSecond());
-		Assert.assertEquals("+08:00",dt.getZone().toString());
+		Assert.assertEquals("+08:00", dt.getZone().toString());
 	}
-	
+
 	@Test
-	public void testPrintDate(){
-		DateTime dt = ISODateTimeFormat.dateOptionalTimeParser().parseDateTime("2013-05-12");
+	public void testPrintDate() {
+		DateTime dt = ISODateTimeFormat.dateOptionalTimeParser().parseDateTime(
+				"2013-05-12");
 		String result = DateTimeAdapter.printDate(dt);
 		Assert.assertNotNull(result);
 		Assert.assertEquals("2013-05-12", result);
 	}
-	
+
 	@Test
-	public void testPrintDateTime(){
-		DateTime dt = ISODateTimeFormat.dateTimeParser().withOffsetParsed().parseDateTime("2013-05-12T23:11:20+08:00");
+	public void testPrintDateTime() {
+		DateTime dt = ISODateTimeFormat.dateTimeParser().withOffsetParsed()
+				.parseDateTime("2013-05-12T23:11:20+08:00");
 		String result = DateTimeAdapter.printDateTime(dt);
 		Assert.assertNotNull(result);
 		Assert.assertEquals("2013-05-12T23:11:20.000+08:00", result);
 	}
-	
+
 	@Test
-	public void testDateToXMLGregorianCalendar(){
-		
+	public void testDateToXMLGregorianCalendar() {
+		XMLGregorianCalendar dt = DateTimeAdapter.dateToXMLGregorianCalendar(ISODateTimeFormat.dateOptionalTimeParser().parseDateTime("2013-05-12"));
+		Assert.assertNotNull(dt);
+		Assert.assertTrue(dt instanceof XMLGregorianCalendar);
+		Assert.assertEquals(2013, dt.getYear());
+		Assert.assertEquals(5, dt.getMonth());
+		Assert.assertEquals(12, dt.getDay());
+	}
+
+	@Test
+	public void testDateTimeToXMLGregorianCalendar() {
+		XMLGregorianCalendar dt = DateTimeAdapter.dateTimeToXMLGregorianCalendar(ISODateTimeFormat.dateTimeParser().parseDateTime("2013-05-12T10:11:20"));
+		Assert.assertNotNull(dt);
+		Assert.assertTrue(dt instanceof XMLGregorianCalendar);
+		Assert.assertEquals(2013, dt.getYear());
+		Assert.assertEquals(5, dt.getMonth());
+		Assert.assertEquals(12, dt.getDay());
+		Assert.assertEquals(10, dt.getHour());
+		Assert.assertEquals(11, dt.getMinute());
+		Assert.assertEquals(20, dt.getSecond());
 	}
 	
 	@Test
-	public void testDateTimeToXMLGregorianCalendar(){
-		
+	public void testXMLGregorianCalendarToDateTime() throws DatatypeConfigurationException{
+		XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        xmlGregorianCalendar.setDay(12);
+        xmlGregorianCalendar.setMonth(5);
+        xmlGregorianCalendar.setYear(2013);
+        
+		DateTime dt = DateTimeAdapter.XMLGregorianCalendarToDateTime(xmlGregorianCalendar);
+		Assert.assertNotNull(dt);
+		Assert.assertEquals(2013, dt.getYear());
+		Assert.assertEquals(5, dt.getMonthOfYear());
+		Assert.assertEquals(12, dt.getDayOfMonth());
+		Assert.assertEquals(0, dt.getHourOfDay());
+		Assert.assertEquals(0, dt.getMinuteOfHour());
+		Assert.assertEquals(0, dt.getSecondOfMinute());
+		Assert.assertEquals(0, dt.getMillisOfSecond());
 	}
 }
--- EasyMockDemo/src/com/expedia/e3/insurance/tools/HttpToolsTest.java
+++ EasyMockDemo/src/com/expedia/e3/insurance/tools/HttpToolsTest.java
@@ -1,19 +1,11 @@
 package com.expedia.e3.insurance.tools;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.easymock.EasyMock;
 
-
-
-
-
-
-
+import com.expedia.e3.insurance.tools.HttpTools;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URLConnection;
 import java.io.ByteArrayOutputStream;
@@ -21,91 +13,126 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
+
 /**
  * 
- * TODO v-yangli Describe HttpToolsTest
+ * Unit Test for Class HttpTools
  * 
  * @author <a href="mailto:v-yangli@expedia.com">v-yangli</a>
- *
+ * 
  */
 public class HttpToolsTest
 {
-	private final static String data = "test1\ntest2\ntest3\n";
-	private static URLConnection con;
-	private static ByteArrayInputStream arrays;
-	private static ServerSocket serverSocket;
-	
-	@BeforeClass
-    public static void setUp() throws IOException {
-    	arrays = new ByteArrayInputStream(data.getBytes("UTF8"));
-		con = EasyMock.createMock(URLConnection.class);
+    private final static String responseHeadLine = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
+    private final static String data = "test1\ntest2\ntest3\n";
+    private URLConnection con;
+    private ByteArrayInputStream arrays;
+    private ServerSocket serverSocket;
+
+    public void mockObject() throws IOException
+    {
+        arrays = new ByteArrayInputStream(data.getBytes("UTF8"));
+        con = EasyMock.createMock(URLConnection.class);
         EasyMock.expect(con.getOutputStream()).andReturn(new ByteArrayOutputStream()).anyTimes();
         EasyMock.expect(con.getInputStream()).andReturn(arrays).anyTimes();
         EasyMock.replay(con);
-        System.out.println("test");
-        
-        new Thread(){
-        	public void run(){
-        		try {
-					serverSocket = new ServerSocket(8088);
-					serverSocket.setReuseAddress(true);
-					Socket socket = serverSocket.accept();
-	                OutputStream out = socket.getOutputStream();
-	                out.write(data.getBytes("UTF8"));
-	                out.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-        	}
-        }.run();
     }
-    
-	@AfterClass
-	public static void tearDown() throws IOException {
-		if(serverSocket != null){
-			serverSocket.close();
-		}
-	}
-	
+
+    public void setUp(final Boolean exceptionFlag)
+    {
+        new Thread()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        serverSocket = new ServerSocket();
+                        serverSocket.setReuseAddress(true);
+                        serverSocket.bind(new InetSocketAddress(8089));
+                        Socket clientSocket = serverSocket.accept();
+                        OutputStream out = clientSocket.getOutputStream();
+                        if (!exceptionFlag)
+                            out.write(responseHeadLine.getBytes("UTF8"));
+                        out.write(data.getBytes("UTF8"));
+                        out.flush();
+                        try
+                        {
+                            Thread.sleep(500);
+                        }
+                        catch (Exception e)
+                        {
+
+                        }
+                        out.close();
+                        clientSocket.close();
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+    }
+
+    public void tearDown() throws IOException
+    {
+        if (serverSocket != null && !serverSocket.isClosed())
+        {
+            serverSocket.close();
+        }
+    }
+
     @Test
-    public void testDoPost() throws IOException {
-    	String expected = "test1test2test3";
-    	String result = HttpTools.doPost(con, "test");
-    	Assert.assertNotNull(result);
-    	Assert.assertEquals(expected,result);
+    public void testDoPost() throws IOException
+    {
+        mockObject();
+        String expected = "test1test2test3";
+        String result = HttpTools.doPost(con, "test");
+        Assert.assertNotNull(result);
+        Assert.assertEquals(expected, result);
     }
-    
+
     @Test
-    public void testDoGet() throws IOException {
-    	String expected = "test1test2test3";
-    	String result = HttpTools.doGet(con);
-    	Assert.assertNotNull(result);
-    	Assert.assertEquals(expected,result);
+    public void testDoGet() throws IOException
+    {
+        mockObject();
+        String expected = "test1test2test3";
+        String result = HttpTools.doGet(con);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(expected, result);
     }
-    
+
     @Test
-    public void testPostRequestByGlobalURLConnectionWithHttp() throws IOException{
-//    	URL url = EasyMock.createMock(URL.class);
-//    	HttpURLConnection httpCon = EasyMock.createMock(HttpURLConnection.class);
-//    	EasyMock.expect(url.openConnection()).andReturn(httpCon).anyTimes();
-//    	EasyMock.replay();
-    	String expected = "test1test2test3";
-    	while(serverSocket==null);
-    	String result = HttpTools.postRequestByGlobalURLConnection("http://localhost:8088", "request=help", "GET", -1, -1);
-    	Assert.assertNotNull(result);
-    	Assert.assertEquals(expected,result);
+    public void testPostRequestByGlobalURLConnectionWithHttpGet() throws IOException
+    {
+        setUp(false);
+        String expected = "test1test2test3";
+        String result = HttpTools.postRequestByGlobalURLConnection("http://localhost:8089/test", "request=help", "GET", -1, -1);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(expected, result);
+        tearDown();
     }
-    
-//    @Test
-//    public void testPostRequestByGlobalURLConnectionWithHttps() throws IOException{
-//    	URL url = EasyMock.createMock(URL.class);
-//    	HttpsURLConnection httpsCon = EasyMock.createMock(HttpsURLConnection.class);
-//    	EasyMock.expect(url.openConnection()).andReturn(httpsCon).anyTimes();
-//    	EasyMock.replay();
-//    	String expected = "test1test2test3";
-//    	String result = HttpTools.postRequestByGlobalURLConnection("https://testforfun.com", "request=help", "get", 45, 43);
-//    	Assert.assertNotNull(result);
-//    	Assert.assertEquals(expected,result);
-//    }
+
+    @Test
+    public void testPostRequestByGlobalURLConnectionWithHttpPost() throws IOException
+    {
+        setUp(false);
+        String expected = "test1test2test3";
+        String result = HttpTools.postRequestByGlobalURLConnection("http://localhost:8089/test", "request=help", "POST", 5000, 5000);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(expected, result);
+        tearDown();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testPostRequestByGlobalURLConnectionWithException() throws IOException
+    {
+        setUp(true);
+        String expected = "test1test2test3";
+        String result = HttpTools.postRequestByGlobalURLConnection("http://localhost:8089/test", "request=help", "POST", -1, -1);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(expected, result);
+        tearDown();
+    }
+
 }
