c66c5b97d04e129fcfa7fdde58db76f67fa20c62
P0043626 <P0043626@LP0043626.sz.vsg.com>
2014-07-23 16:36:50 +0800

    add jquery ajax deferred example

--- spring_rest_freemarker/src/xpadro/tutorial/rest/controller/ProductAddFormController.java
+++ spring_rest_freemarker/src/xpadro/tutorial/rest/controller/ProductAddFormController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,6 +19,7 @@ import xpadro.tutorial.rest.repository.WarehouseRepository;
 public class ProductAddFormController {
 	
 	@Autowired
+	@Qualifier("warehouseRepository")
 	private WarehouseRepository warehouseRepository;
 	
 	@RequestMapping(value="/Product", method=RequestMethod.GET)
