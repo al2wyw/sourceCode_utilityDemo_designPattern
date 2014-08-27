271321bf4ca797b687e4899af714216fa97db973
P0043626 <P0043626@LP0043626.sz.vsg.com>
2014-08-27 09:09:46 +0800

    remove piano.xsd and add multiple xsd validation

--- JAXBDemo/src/test/PersonValidation.java
+++ JAXBDemo/src/test/PersonValidation.java
@@ -1,25 +1,24 @@
 package test;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import javax.xml.transform.Result;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.stream.StreamSource;
+
+
 
 import org.xml.sax.SAXException;
 public class PersonValidation {
 
-	public static void main(String[] args) throws FileNotFoundException, XMLStreamException, SAXException {
-		XMLInputFactory xf=XMLInputFactory.newInstance();
-		XMLStreamReader sr = xf.createXMLStreamReader(new FileInputStream(new File("persons.xml")));
-		if(sr != null){
+	public static void main(String[] args) throws SAXException, IOException {
+		StreamSource ss = new StreamSource(new FileInputStream(new File("persons.xml")));
+		if(ss != null){
 			SchemaFactory sf = SchemaFactory.newInstance(javax.xml.XMLConstants.W3C_XML_SCHEMA_NS_URI);
-			Validator va = sf.newSchema(new File("person.xsd")).newValidator();
-			va.validate(sr.);
+			Validator va = sf.newSchema(new File("address.xsd")).newValidator();//use import to solve multiple xsd files for one xml file
+			va.validate(ss);
+			System.out.println("test");
 		}
 	}
 
