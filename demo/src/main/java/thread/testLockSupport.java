009e52441d9574e71328d6f6be368960b1931136
励扬 <johnny.ly@alibaba-inc.com>
2016-06-04 18:50:16 +0800

    interrupt parked thread

--- demo/src/main/java/thread/testLockSupport.java
+++ demo/src/main/java/thread/testLockSupport.java
@@ -8,8 +8,13 @@ import java.util.concurrent.locks.LockSupport;
  * Created by johnny.ly on 2016/5/31.
  */
 public class testLockSupport {
-    //lockSupport park will be interrupt
+    //lockSupport park will be interrupt and not throw interrupted exception
     public static void main(String[] args) throws Exception{
+        //testPark();
+        testInterruptPark();
+    }
+
+    private static void testPark(){
         ExecutorService executorService = Executors.newFixedThreadPool(10);
         Object lock = new Object();
         final Thread t = Thread.currentThread();
@@ -36,5 +41,31 @@ public class testLockSupport {
         System.out.println("main thread park" + System.currentTimeMillis());
         LockSupport.park(lock);//this will  wait
         System.out.println("main thread wake up" + System.currentTimeMillis());
+    }
+
+    private static void testInterruptPark(){
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Object lock = new Object();
+        final Thread t = Thread.currentThread();
+        executorService.submit(new Runnable() {
+            public void run() {
+                System.out.println("thread start" + System.currentTimeMillis());
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+
+                }
+                if (!t.isInterrupted()) {
+                    t.interrupt();
+                    System.out.println("interrupt main thread" + System.currentTimeMillis());
+                }
+            }
+        });
+        System.out.println("main thread park" + System.currentTimeMillis());
+        LockSupport.park(lock);
+        System.out.println("main thread wake up" + System.currentTimeMillis());
+        if(t.isInterrupted()){
+            System.out.println("main thread interrupted" + System.currentTimeMillis());
+        }
     }
 }
