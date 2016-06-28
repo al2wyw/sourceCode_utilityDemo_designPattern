15f75ea4d2923f31e1677c429929d3de4c919ecf
励扬 <johnny.ly@alibaba-inc.com>
2016-06-28 20:31:34 +0800

    filter asynchronize

--- demo/src/main/java/demo/testFluentIterable.java
+++ demo/src/main/java/demo/testFluentIterable.java
@@ -1,7 +1,9 @@
 package demo;
 
+import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Iterables;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,5 +26,23 @@ public class testFluentIterable {
         System.out.println(res.get(0));
         res.get(0).append("t");
         System.out.println(res.get(0));
+
+        List<Integer> test = FluentIterable.from(Arrays.asList(new StringBuilder("test1"), new StringBuilder("test2"), new StringBuilder("test3"), new StringBuilder("start1")))
+                .transform(new Function<StringBuilder, Integer>() {
+                    public Integer apply(StringBuilder input) {
+                        return Integer.valueOf(input.toString().substring(input.length() - 1, input.length()));
+                    }
+                })
+                .toList();
+        System.out.println(test.size());
+        Iterable<Integer> filteredList = Iterables.filter(test, new Predicate<Integer>() {
+            public boolean apply(Integer input) {
+                System.out.println("filter out:" + input);//没有"Iterable<Integer> filteredList ="不能打印,竟然是异步调用apply
+                return true;
+            }
+        });
+        for(int i:filteredList){
+            System.out.println("test:" + i);
+        }
     }
 }
