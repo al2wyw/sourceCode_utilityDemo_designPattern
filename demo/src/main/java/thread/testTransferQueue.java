c5beb6abc28c6c1c9ac07280c53dd72f7a2bbf62
励扬 <johnny.ly@alibaba-inc.com>
2016-07-08 11:39:01 +0800

    transfer has capacity while synchronized has not

--- demo/src/main/java/thread/testTransferQueue.java
+++ demo/src/main/java/thread/testTransferQueue.java
@@ -6,7 +6,13 @@ import java.util.concurrent.*;
  * Created by johnny.ly on 2016/2/4.
  */
 public class testTransferQueue {
-    private static ConcurrentLinkedQueue<String> log = new ConcurrentLinkedQueue<String>();
+    private static ConcurrentLinkedQueue<String> log = new ConcurrentLinkedQueue<String>(){
+        @Override
+        public boolean add(String s) {
+            return super.add(System.currentTimeMillis()+":"+s);
+        }
+    };
+
     public static void main(String args[]) throws Exception{
         testTransfer();
         //testSynchronize();//SynchronousQueue not use too much, for threadpoolexecutor only
@@ -46,12 +52,13 @@ public class testTransferQueue {
     private static void testTransfer()throws Exception{
         final TransferQueue<String> transferQueue = new LinkedTransferQueue<String>();
         transferQueue.put("test0");
+        transferQueue.put("test2");
         ExecutorService executorService = Executors.newCachedThreadPool();
         executorService.execute(new Runnable() {
             public void run() {
                 try {
                     // 要两个take,才能wake up
-                    transferQueue.transfer("test1");//put it into queue first and then wait for consumer
+                    transferQueue.transfer("test1");//put it into queue first and then wait for consumer to retrieve all of elements including this one
                     log.add("子线程完成");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -61,6 +68,7 @@ public class testTransferQueue {
 
         String test = transferQueue.take();
         log.add("主线程完成获取 " + test);
+        Thread.sleep(1000);
         test = transferQueue.take();
         log.add("主线程完成获取 " + test);
         Thread.sleep(1000);
