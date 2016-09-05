ef9930f250957ece8c7a83580ccd65eb19744c8e
励扬 <johnny.ly@alibaba-inc.com>
2016-09-05 09:16:37 +0800

    timezone study

--- demo/src/main/java/demo/date_parse_test.java
+++ demo/src/main/java/demo/date_parse_test.java
@@ -1,6 +1,9 @@
 package demo;
 
+import com.google.common.collect.Lists;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -19,7 +22,13 @@ public class date_parse_test {
     public static void main(String[] args) throws Exception{
         normal1();
         normal2();
+        normal3();
+        normal4();
+        normal5();
         System.out.println(TimeZone.getDefault().toString());
+
+        int base = 60*60*1000;
+        System.out.println(Lists.newArrayList(TimeZone.getAvailableIDs(-8 * base)));
     }
 
     private static void normal1() throws Exception{
@@ -38,6 +47,60 @@ public class date_parse_test {
         SuccessTimeFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         Date time = SuccessTimeFormat.parse(test);
         if(time!=null) {
+            System.out.println(SuccessTimeFormat.format(time));
+            System.out.println(TimeFormat.format(time));
+        }
+    }
+
+    private static void normal3() throws Exception{
+        SimpleDateFormat SuccessTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssXXX");
+        String test = "2016-09-01 20:00:00-08:00";
+        //SuccessTimeFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date time = SuccessTimeFormat.parse(test);
+        if(time!=null) {
+            System.out.println(SuccessTimeFormat.format(time));//SimpleDateFormat 不处理 夏令时
+            System.out.println(TimeFormat.format(time));
+        }
+    }
+
+    private static void normal4() throws Exception{
+        SimpleDateFormat SuccessTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String test = "2016-11-01 20:00:00";
+        TimeZone tz = TimeZone.getTimeZone("GMT-08:00");
+        SuccessTimeFormat.setTimeZone(tz);
+        Date time = SuccessTimeFormat.parse(test);
+        if(time!=null) {
+            System.out.println(tz.inDaylightTime(time));//timezone 不处理 夏令时 ??? 可以的请看normal5
+            System.out.println(SuccessTimeFormat.format(time));
+            System.out.println(TimeFormat.format(time));
+        }
+    }
+
+    private static void normal5() throws Exception{
+        //-Duser.timezone=America/North_Dakota/Center
+        TimeZone tz = TimeZone.getTimeZone("GMT-08:00");// TimeZone -> ZoneInfo -> ZoneInfoFile to find the tz setup file, if not found,create a custom tz with a few fields being set up(no daylight setup)
+        System.out.println("tz: " + tz);
+
+        int offset = tz.getRawOffset();
+        System.out.println("raw offset: " + offset);
+
+        int dstSavings = tz.getDSTSavings();
+        System.out.println("dstSavings: " + dstSavings);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Calendar cal = Calendar.getInstance();
+        String msg = "[" + sdf.format(cal.getTime()) + "] " + cal.getTime();
+        msg += ", offset: " + TimeZone.getDefault().getOffset(cal.getTimeInMillis());
+        System.out.println(msg);
+
+
+        SimpleDateFormat SuccessTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String test = "2016-11-01 20:00:00";
+        SuccessTimeFormat.setTimeZone(tz);
+        Date time = SuccessTimeFormat.parse(test);
+        if(time!=null) {
+            System.out.println(tz.useDaylightTime());
             System.out.println(SuccessTimeFormat.format(time));
             System.out.println(TimeFormat.format(time));
         }
--- demo/src/main/java/httpclient/testHttpClient.java
+++ demo/src/main/java/httpclient/testHttpClient.java
@@ -1,14 +1,18 @@
 package httpclient;
 
 import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.config.ConnectionConfig;
 import org.apache.http.config.MessageConstraints;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
@@ -35,7 +39,7 @@ public class testHttpClient {
                 .build();
         RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(5000)
-                .setSocketTimeout(4000)
+                .setSocketTimeout(5000)
                 .setConnectTimeout(3000)
                 .build();
         PoolingHttpClientConnectionManager manager =  new PoolingHttpClientConnectionManager();
@@ -57,7 +61,45 @@ public class testHttpClient {
         HttpGet get = new HttpGet("http://baidu.com");
         HttpResponse response = httpClient.execute(get);
         if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-            System.out.print("good");
+            System.out.println("good");
         }
+        HttpPost post = new HttpPost("https://interface.demo.gta-travel.com/rbscnapi/RequestListenerServlet");
+        String postData = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<Request>\n" +
+                "<Source>\n" +
+                "<RequestorID Client=\"93\" EMailAddress=\"XML@ALITRIP.COM\" Password=\"PASS\"/>\n" +
+                "<RequestorPreferences Country=\"CN\" Currency=\"GBP\" Language=\"en\">\n" +
+                "<RequestMode>SYNCHRONOUS</RequestMode>\n" +
+                "</RequestorPreferences>\n" +
+                "</Source>\n" +
+                "<RequestDetails>\n" +
+                "<SearchHotelPricePaxRequest>\n" +
+                "<ItemDestination DestinationCode = \"NCE\" DestinationType = \"city\"/>\n" +
+                "<ItemCodes>\n" +
+                "<ItemCode>ACO</ItemCode>\n" +
+                "<ItemCode>BEL</ItemCode>\n" +
+                "<ItemCode>COM</ItemCode>\n" +
+                "</ItemCodes>\n" +
+                "<PeriodOfStay>\n" +
+                "<CheckInDate>2016-11-12</CheckInDate>\n" +
+                "<Duration>2</Duration>\n" +
+                "</PeriodOfStay>\n" +
+                "<IncludePriceBreakdown/>\n" +
+                "<IncludeChargeConditions/>\n" +
+                "<PaxRooms>\n" +
+                "<PaxRoom Adults=\"2\" Cots=\"0\" RoomIndex=\"1\"/>\n" +
+                "</PaxRooms>\n" +
+                "</SearchHotelPricePaxRequest>\n" +
+                "</RequestDetails>\n" +
+                "</Request>";
+        StringEntity entity = new StringEntity(postData, ContentType.APPLICATION_XML);
+        entity.setContentEncoding("UTF-8");
+        post.setEntity(entity);
+        HttpResponse response1 = httpClient.execute(post);
+        HttpEntity responseEntity = response1.getEntity();
+        if(responseEntity!=null){
+            System.out.println(responseEntity.getContentType().getValue());
+        }
+        //connection keep alive to reuse the connection ?
     }
 }
