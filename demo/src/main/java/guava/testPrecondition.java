bdee750c7a3956c8942110d63fa6c0e0aeb9b6ab
johnny.ly <johnny.ly@alibaba-inc.com>
2017-01-17 15:50:45 +0800

    test map fiter entry

--- demo/src/main/java/guava/testPrecondition.java
+++ demo/src/main/java/guava/testPrecondition.java
@@ -1,6 +1,11 @@
 package guava;
 
+import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,6 +17,15 @@ import com.google.common.base.Preconditions;
 public class testPrecondition {
     public static void main(String[] args) throws Exception{
         String test = null;
-        Preconditions.checkArgument(test == null,"test");
+        Preconditions.checkArgument(test == null, "test");
+        Map<Integer,Integer> map = Maps.newHashMap();
+        map.put(1,1);
+        map.put(2,2);
+        map = Maps.filterEntries(map, new Predicate<Map.Entry<Integer, Integer>>() {
+            public boolean apply(Map.Entry<Integer, Integer> input) {
+                return input.getValue() == 1;
+            }
+        });
+        System.out.println(Joiner.on(",").join(map.keySet()));
     }
 }
