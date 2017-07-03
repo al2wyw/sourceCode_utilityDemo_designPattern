5f2ed3d35e16bef9ba0c9daae2b2b0e29cbc3f9f
al2wyw <yl0863758497@gmail.com>
2017-07-03 10:23:18 +0800

    add comments builder

--- demo/src/main/java/demo/testCacheBuilder.java
+++ demo/src/main/java/demo/testCacheBuilder.java
@@ -6,7 +6,7 @@ package demo;
 public class testCacheBuilder {
     //Cache -> LoadingCache -> LocalLoadingCache
     //Cache -> LocalManualCache -> LocalLoadingCache
-    //对应于cache builder的两种build方法所产生的cache
+    //cache builder的两种build方法分别产生LocalManualCache和LocalLoadingCache
     //Cache的所有方法委托于LocalCache类(并不继承于Cache)
     //cache的加载依赖于CacheLoader
     public static void main(String[] args) throws Exception {
