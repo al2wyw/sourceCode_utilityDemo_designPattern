3bd413cac4c76cbd11ee9b4fe74ee17e5f9f88c8
al2wyw <yl0863758497@gmail.com>
2017-07-03 09:30:52 +0800

    add cache loader

--- demo/src/main/java/demo/testCacheBuilder.java
+++ demo/src/main/java/demo/testCacheBuilder.java
@@ -4,10 +4,11 @@ package demo;
  * Created by johnny.ly on 2016/6/26.
  */
 public class testCacheBuilder {
-    //Cache -> LoadingCache
-    //LocalManualCache -> LocalLoadingCache
+    //Cache -> LoadingCache -> LocalLoadingCache
+    //Cache -> LocalManualCache -> LocalLoadingCache
     //对应于cache builder的两种build方法所产生的cache
     //Cache的所有方法委托于LocalCache类(并不继承于Cache)
+    //cache的加载依赖于CacheLoader
     public static void main(String[] args) throws Exception {
 
     }
