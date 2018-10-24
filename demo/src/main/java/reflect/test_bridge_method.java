cede9ca39a4c4519432aa5cb9a3b43102ca6456f
johnny.ly <johnny.ly@alibaba-inc.com>
2018-10-24 19:21:34 +0800

    bridge method

--- demo/src/main/java/reflect/test_bridge_method.java
+++ demo/src/main/java/reflect/test_bridge_method.java
@@ -21,6 +21,9 @@ public class test_bridge_method {
 		arr = k.getMethods();//会返回bridge方法
 		System.out.println(arr.length);
 		Arrays.asList(arr).forEach(ar -> System.out.println(ar.getName()));
+
+		E<String> e = new E<>();//没有子类，只有实例，不会生成bridge方法
+		e.setValue("test");
 	}
 
 }
@@ -36,4 +39,16 @@ class D extends C<String> {
 	//编译时根据模板C的方法多生成一个synthetic bridge 方法:
 	//Object id(Object x){ return id((String)x);}
 	//bridge方法的实现都是这个样子，cast后调用非bridge方法
+}
+
+class E<e>{
+	private e value;
+
+	public e getValue() { // -> Object getValue()
+		return value;
+	}
+
+	public void setValue(e value) { // setValue(Object x)
+		this.value = value;
+	}
 }
