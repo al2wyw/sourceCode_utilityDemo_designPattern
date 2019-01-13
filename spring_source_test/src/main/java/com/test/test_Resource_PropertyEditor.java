d1dfdcb3f480cdd434b062398b23b86e82ea2ccb
johnny.ly <johnny.ly@alibaba-inc.com>
2019-01-13 13:19:27 +0800

    resouces finding

--- spring_source_test/src/main/java/com/test/test_Resource_PropertyEditor.java
+++ spring_source_test/src/main/java/com/test/test_Resource_PropertyEditor.java
@@ -1,19 +1,38 @@
 package com.test;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.ServletContextResource;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.support.ServletContextResource;
-
+import java.util.Arrays;
+/**
+ *  classpath:  cp下的所有jar包和classes目录都会查找直到找到一个路径完全匹配resource
+ *  classpath*: cp下的所有jar包和classes目录都会查找并返回多个路径完全匹配resource (路径也可以含有*)
+ * */
 @Component
-public class test_Resource_PropertyEditor {
+public class test_Resource_PropertyEditor implements ApplicationContextAware{
 	@Value("classpath:springmvc.xml")//ResourceEditorRegistrar
 	private Resource temple;
+
+	@Value("classpath*:META-INF/LICENSE")
+	private Resource[] files;
+
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
 	public void call() {
 		try{
 			if(ServletContextResource.class.isInstance(temple)){
@@ -27,6 +46,15 @@ public class test_Resource_PropertyEditor {
 			char[] cbuf=new char[1024];
 			r.read(cbuf);
 			System.out.println(cbuf);
+
+			Resource res = applicationContext.getResource("classpath:META-INF/LICENSE"); // DefaultResourceLoader
+			System.out.println(res);
+			System.out.println(res.getURL());//classLoader.getResource("META-INF/LICENSE")
+			System.out.println("resources========");
+			Resource[] test = applicationContext.getResources("classpath*:META-INF/LICENSE");//PathMatchingResourcePatternResolver
+			//最终调用 classLoader.getResources("META-INF/LICENSE")
+			Arrays.stream(test).forEach(System.out::println);
+
 		}catch(IOException e){
 			e.printStackTrace();
 		}
