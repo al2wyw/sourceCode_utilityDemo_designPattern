39df47b18cde0e4959eac5ccf059920d1fa3483d
johnny.ly <johnny.ly@alibaba-inc.com>
2019-04-11 20:22:27 +0800

    con hash map put late

--- demo/src/main/java/thread/ConHashMapTest.java
+++ demo/src/main/java/thread/ConHashMapTest.java
@@ -1,6 +1,10 @@
 package thread;
 
+import com.google.common.base.Stopwatch;
+
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,12 +14,29 @@ import java.util.concurrent.ConcurrentHashMap;
  * Desc:
  *       i < 0 || i >= n || i + n >= nextn maybe a bug, i >= n || i + n >= nextn can not achieve
  *       sc == rs + 1 || sc == rs + MAX_RESIZERS is a bug, rs should be resizeStamp(n) << RESIZE_STAMP_SHIFT
+ *
  */
 public class ConHashMapTest {
+
     public static void main(String[] args) throws Exception{
+        CountDownLatch latch = new CountDownLatch(1);
         ConcurrentHashMap<String ,Integer> values = new ConcurrentHashMap<>(4);
-        for(int i = 0; i< 1000; i++) {
+
+        new Thread(() -> {
+            try {
+                latch.await();
+                Stopwatch stopwatch = Stopwatch.createStarted();//3145728 table[4194303] - > 1769191
+                values.put("1769191",100);
+                System.out.println(stopwatch.stop().elapsed(TimeUnit.MICROSECONDS));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }).start();
+
+        for(int i = 0; i< 3145727; i++) {
             values.put(String.valueOf(i), i);
         }
+        latch.countDown();
+        values.put("res",343444);//100 ns VS 100000 ns
     }
 }
