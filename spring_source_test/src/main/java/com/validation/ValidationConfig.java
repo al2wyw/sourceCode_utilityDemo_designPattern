e534eaebbda78ffff9a039e89dcea99a66d2fcf4
johnny.ly <johnny.ly@alibaba-inc.com>
2019-04-19 17:54:57 +0800

    add validator comment

--- spring_source_test/src/main/java/com/validation/ValidationConfig.java
+++ spring_source_test/src/main/java/com/validation/ValidationConfig.java
@@ -15,6 +15,18 @@ import java.util.Locale;
  * Date: 2019/1/13
  * Time: 19:57
  * Desc:
+ Bean Validation 1.1当前实现是Hibernate validator 5，且spring4才支持。接下来我们从以下几个方法讲解Bean Validation 1.1，当然不一定是新特性：
+ 集成Bean Validation 1.1到SpringMVC
+ 分组验证、分组顺序及级联验证 (比较厉害)
+ 消息中使用EL表达式
+ 方法参数/返回值验证
+ 自定义验证规则
+ 类级别验证器
+ 脚本验证器
+ cross-parameter，跨参数验证
+ 混合类级别验证器和跨参数验证器
+ 组合多个验证注解
+ 本地化
  */
 @Configuration
 public class ValidationConfig {
@@ -24,6 +36,7 @@ public class ValidationConfig {
         return new MethodValidationPostProcessor();
     }
 
+    //配置 Validator, 可以注入到其他类里进行手动校验
     /*@Bean
     public LocalValidatorFactoryBean LocalValidatorFactoryBean(){
         LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
