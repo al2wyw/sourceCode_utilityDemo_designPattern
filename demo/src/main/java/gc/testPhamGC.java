6a8594b6f294635e385addaaeca1af340806e511
johnny.ly <johnny.ly@alibaba-inc.com>
2019-04-26 17:06:24 +0800

    pham gc comments

--- demo/src/main/java/gc/testPhamGC.java
+++ demo/src/main/java/gc/testPhamGC.java
@@ -2,11 +2,10 @@ package gc;
 
 import demoObject.BigObject;
 import demoObject.SmallObject;
-import utils.GCUtils;
 
+import java.lang.ref.PhantomReference;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
-import java.lang.ref.PhantomReference;
 import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,7 +13,7 @@ import java.util.concurrent.locks.LockSupport;
 
 /**
  * Created by johnny.ly on 2016/6/26.
- * 使用sa来查看object
+ *
  */
 public class testPhamGC {
 
@@ -36,11 +35,11 @@ public class testPhamGC {
     public static void main(String[] args) throws Exception{
 
         //如果不放入object1s, 那么 full gc 时PhantomReference本身会被回收掉，也就不会被放入queue
-        SmallObject smallObject = new SmallObject();
-        object1s.add(new PhantomReference<Object>(smallObject,queue));
-        object1s.add(new PhantomReference<Object>(new SmallObject(),queue));
-        object1s.add(new PhantomReference<Object>(new SmallObject(),queue));
-        smallObject = null; //young gc 为什么没有回收 smallObject, PhantomReference的引用对象需要至少两次gc,必须queue.poll后才能在第二次gc时被回收
+        BigObject bigObject = new BigObject();
+        object1s.add(new PhantomReference<Object>(bigObject,queue));
+        object1s.add(new PhantomReference<Object>(new BigObject(),queue));
+        object1s.add(new PhantomReference<Object>(new BigObject(),queue));
+        bigObject = null; //young gc 为什么没有回收 bigObject ??? PhantomReference强引用对象, 必须解除引用后才能在第二次gc时被回收
 
         for(int i=0;i<1000;i++){
             BigObject o = new BigObject();
@@ -51,6 +50,7 @@ public class testPhamGC {
             while((r = queue.poll())!=null){
                 PhantomReference<Object> mr = (PhantomReference<Object>)r;
                 System.out.println("object from queue " + mr.get() + " " + ref.get(mr));
+                object1s.remove(mr);//没有这个的话gc后内存还是很高
             }
             LockSupport.parkNanos(1000000000L);
         }
