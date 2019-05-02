6f3a7dda08c37bbe1c10582af0acfa68abd2ab1e
win10 <al2shi@163.com>
2019-05-02 11:53:07 +0800

    remove easy mock demo

--- EasyMockDemo/src/org/example/person/package-info.java
+++
@@ -1,4 +0,0 @@
-@XmlSchema(namespace = "http://www.example.org/person", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.example.person;
-import javax.xml.bind.annotation.XmlSchema;
-
--- EasyMockDemo/src/test1/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package test1;
-
-public class HelloWorld {
-	private User user = new User("user");	
-	public User getUser(){
-		return this.user;
-	}
-	
-}
