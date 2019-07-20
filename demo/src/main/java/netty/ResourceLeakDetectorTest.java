3bc04287820d64fbd7f351994f3f068e4a9f0f61
win10 <al2shi@163.com>
2019-07-20 12:10:42 +0800

    add logback config

--- demo/src/main/java/netty/ResourceLeakDetectorTest.java
+++ demo/src/main/java/netty/ResourceLeakDetectorTest.java
@@ -10,23 +10,27 @@ import java.io.FileInputStream;
  * User: win10
  * Date: 2019/5/18
  * Time: 22:22
- * Desc:
+ * Desc: -Xms10M -Xmx10M -Xmn8m -XX:SurvivorRatio=8
  */
 public class ResourceLeakDetectorTest {
 
     private static ResourceLeakDetector<FileInputStream> detector = new ResourceLeakDetector<>(FileInputStream.class);
 
     public static void main(String args[]) throws Exception{
-        test();
-        System.gc();
-        Thread.sleep(10000);
+        ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
+        ResourceLeakDetectorTest test = new ResourceLeakDetectorTest();
+        test.test();
+        for(int i = 0; i < 10; i++){
+            byte[] gc = new byte[1024*1024];
+        }
+        test.test();//detector.open 的时候才去检查是否泄漏，所以要多调用一次
     }
 
-    private static void test() throws Exception{
+    private void test() throws Exception{
         String basePath = ResourceLeakDetectorTest.class.getClassLoader().getResource("").getPath();
         FileInputStream fin = new FileInputStream(basePath + "gc/TestCMS.class");
         ResourceLeak leak = detector.open(fin);
-        //fin.close();
+        fin.close();
         //leak.close();
     }
 }
