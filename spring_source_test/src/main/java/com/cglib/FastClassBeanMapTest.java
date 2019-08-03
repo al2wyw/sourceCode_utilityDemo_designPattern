1a3552be6364c98aaf0e1b81ec253a03f566432c
johnny.ly <johnny.ly@alibaba-inc.com>
2019-08-03 12:42:08 +0800

    add objenesis test

---
+++ spring_source_test/src/main/java/com/cglib/FastClassBeanMapTest.java
@@ -0,0 +1,12 @@
+package com.cglib;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johnny.ly
+ * Date: 2019/8/3
+ * Time: 12:36
+ * Desc: FastClass & BeanMap 实现方式差不多， 都是动态生成子类代码，再加载代码生成对应的对象
+ *       FastClass 对各个方法的签名进行映射到index，index再映射到对应的字节码上面，然后运行对应的字节码
+ */
+public class FastClassBeanMapTest {
+}
---
+++ spring_source_test/src/main/java/com/objenesis/ObjenesisTest.java
@@ -0,0 +1,35 @@
+package com.objenesis;
+
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johnny.ly
+ * Date: 2019/8/2
+ * Time: 18:17
+ * Desc:
+ */
+public class ObjenesisTest {
+
+    public static void main( String args[] ) throws Exception{
+        //sun hotspot vm运行时会使用ReflectionFactory.newConstructorForSerialization
+        //强依赖运行时的vm: StdInstantiatorStrategy.newInstantiatorOf
+        Objenesis objenesis = new ObjenesisStd(true);
+        Test test = (Test)objenesis.newInstance(Test.class);
+        test.show();
+    }
+
+    public static class Test{
+
+        private int i;
+
+        public Test(int i){
+            this.i = i;
+        }
+
+        public void show(){
+            System.out.println("show " + i);
+        }
+    }
+}
