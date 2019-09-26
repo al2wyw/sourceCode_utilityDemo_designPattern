1307243bd0df0760907e6ba3ac943ae37e582405
johnny.ly <johnny.ly@alibaba-inc.com>
2019-09-26 15:17:12 +0800

    add greys notes

--- spring_source_test/src/main/java/com/instrumentation/InstrumentController.java
+++ spring_source_test/src/main/java/com/instrumentation/InstrumentController.java
@@ -3,10 +3,13 @@ package com.instrumentation;
 import com.componentScan.MyConditional;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.InputStream;
+import java.lang.instrument.ClassDefinition;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.lang.reflect.Field;
@@ -21,6 +24,11 @@ import java.util.List;
  * Desc: -javaagent:D:\agent.jar
  *       -Dsun.reflect.inflationThreshold=0
  *       即使transform成新的方法，也不影响已经被缓存下来的Method的调用
+ *
+ *       redefineClass 先用redefineByteCode替换运行时的字节码，会触发transformer(false)然后触发transformer(true)
+ *       retransformClass 只会触发transformer(true), 类似于回滚操作，
+ *       回滚到当前agent第一次redefineClass或者loadClass的结果(包含transformer(false)的transform效果)，然后再触发transformer(true)
+ *       redefineClass 和 retransformClass 搭配使用比较复杂，参考寒泉子的文章
  */
 @Controller
 @MyConditional(trigger="instrument")
@@ -49,7 +57,7 @@ public class InstrumentController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String add(@RequestParam("flag") boolean flag) {
-        ClassFileTransformer classFileTransformer = new MyClassTransformer();
+        ClassFileTransformer classFileTransformer = new MyClassTransformer(String.valueOf(flag));
         try {
             instrumentation.addTransformer(classFileTransformer, flag);
             transformers.add(classFileTransformer);
@@ -65,7 +73,7 @@ public class InstrumentController {
     public String remove() {
         try {
             Preconditions.checkArgument(transformers.size() > 0);
-            instrumentation.removeTransformer(transformers.get(0));
+            instrumentation.removeTransformer(transformers.remove(0));
         }catch (Exception e){
             e.printStackTrace();
         }
@@ -84,11 +92,31 @@ public class InstrumentController {
         return "ok";
     }
 
+    @RequestMapping(value="inst/red", method= RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String red() {
+        try {
+            InputStream inputStream = TargetClass.class.getResourceAsStream("TargetClass.class");
+            ClassFileTransformer classFileTransformer = new MyClassTransformer("test");
+            byte[] transformedCode = classFileTransformer.transform(Thread.currentThread().getContextClassLoader(),
+                    TargetClass.class.getName().replaceAll("\\.","/"),
+                    TargetClass.class,
+                    TargetClass.class.getProtectionDomain(),
+                    IOUtils.toByteArray(inputStream));
+            ClassDefinition classDefinition = new ClassDefinition(TargetClass.class,transformedCode);
+            instrumentation.redefineClasses(classDefinition);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "ok";
+    }
+
     @RequestMapping(value="inst/rein", method= RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public String rein() {
-        ClassFileTransformer classFileTransformer = new MyClassTransformer();
+        ClassFileTransformer classFileTransformer = new MyClassTransformer("test");
         try {
             instrumentation.addTransformer(classFileTransformer,true);
             instrumentation.retransformClasses(TargetClass.class);
--- spring_source_test/src/main/java/com/instrumentation/MyClassTransformer.java
+++ spring_source_test/src/main/java/com/instrumentation/MyClassTransformer.java
@@ -21,11 +21,17 @@ public class MyClassTransformer implements ClassFileTransformer {
 
     private static ClassPool cp = ClassPool.getDefault();
 
+    private String name;
+
+    public MyClassTransformer(String name) {
+        this.name = name;
+    }
+
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         String classname = TargetClass.class.getName().replaceAll("\\.","/");
         if(classname.equals(className)) {
-            System.out.println("start to transform original class");
+            System.out.println(name + " start to transform original class");
             try {
                 ByteArrayInputStream bin = new ByteArrayInputStream(classfileBuffer);
                 CtClass ctClass = cp.makeClass(bin);
