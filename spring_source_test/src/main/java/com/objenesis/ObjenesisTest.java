2f0bdd023890f399ea938fa284e7742b092f0174
johnny.ly <johnny.ly@alibaba-inc.com>
2019-10-25 21:18:45 +0800

    add objenesis test

--- spring_source_test/src/main/java/com/objenesis/ObjenesisTest.java
+++ spring_source_test/src/main/java/com/objenesis/ObjenesisTest.java
@@ -1,5 +1,6 @@
 package com.objenesis;
 
+import com.cglib.NoArgsContTest;
 import org.objenesis.Objenesis;
 import org.objenesis.ObjenesisStd;
 
@@ -9,14 +10,26 @@ import org.objenesis.ObjenesisStd;
  * Date: 2019/8/2
  * Time: 18:17
  * Desc:
+ *      newConstructorForSerialization 会生成 GeneratedSerializationConstructorAccessor1 extends SerializationConstructorAccessorImpl:
+ *      new           #6                  // class java/lang/Number
+ *      dup
+ *      invokespecial #12                 // Method java/lang/Object."<init>":()V
+ *      areturn
+ *
+ *      sun.reflect.SerializationConstructorAccessorImpl: new B; invokespecial A.init will throw Exception (verification fails)
+ *      but this marker class was originally known to the VM and verification disabled for it and all subclasses
+ *
+ *      @see NoArgsContTest fail test
  */
 public class ObjenesisTest {
 
     public static void main( String args[] ) throws Exception{
-        //sun hotspot vm运行时会使用ReflectionFactory.newConstructorForSerialization, 这是java在反序列化时绕开原类的构造函数时调用的接口
-        //强依赖运行时的vm: StdInstantiatorStrategy.newInstantiatorOf
+        //ObjenesisBase 才是骨干，ObjenesisStd只是为了写死InstantiatorStrategy字段的特种类
         Objenesis objenesis = new ObjenesisStd(true);
+        //强依赖运行时的vm: StdInstantiatorStrategy.newInstantiatorOf
         Test test = (Test)objenesis.newInstance(Test.class);
+        //sun hotspot vm运行时会使用 sun.reflect.ReflectionFactory.newConstructorForSerialization, 这是java在反序列化时绕开原类的构造函数时调用的接口
+
         test.show();
     }
 
