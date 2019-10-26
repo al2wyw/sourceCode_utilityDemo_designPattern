8fb93fe81095f11ac038c03768b2d636880fc75e
johnny.ly <johnny.ly@alibaba-inc.com>
2019-10-26 15:29:17 +0800

    add recycle tet

--- demo/src/main/java/netty/RecyclerTest.java
+++ demo/src/main/java/netty/RecyclerTest.java
@@ -1,14 +1,61 @@
 package netty;
 
+import io.netty.util.Recycler;
+
 /**
  * Created by IntelliJ IDEA.
  * User: win10
  * Date: 2019/5/18
  * Time: 22:37
- * Desc:
+ * Desc:  没弄明白 ...
  */
 public class RecyclerTest {
     public static void main(String args[]) throws Exception{
+        Recycler<Test> recycler = new Recycler<Test>() {
+            @Override
+            protected Test newObject(Handle handle) {
+                System.out.println("create new object");
+                return new Test(handle);
+            }
+        };
+        Test tet = recycler.get();
+        tet.print();
+        tet.setName("test1");
+        tet.print();
+        recycler.recycle(tet, tet.getHandle());
 
+        tet = recycler.get();
+        tet.print();
+        tet.setName("test2");
+        tet.print();
+    }
+
+    public static class Test{
+        private Recycler.Handle handle;
+        private String name;
+
+        public Test(Recycler.Handle handle) {
+            this.handle = handle;
+        }
+
+        public Recycler.Handle getHandle() {
+            return handle;
+        }
+
+        public void setHandle(Recycler.Handle handle) {
+            this.handle = handle;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void print(){
+            System.out.println("name " + name);
+        }
     }
 }
