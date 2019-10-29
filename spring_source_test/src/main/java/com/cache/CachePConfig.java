111fab2a17c6f4d65a22df9df340bc1e6330fe71
johnny.ly <johnny.ly@alibaba-inc.com>
2019-10-29 16:28:14 +0800

    add cache test

---
+++ spring_source_test/src/main/java/com/cache/CachePConfig.java
@@ -0,0 +1,17 @@
+package com.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johnny.ly
+ * Date: 2019/10/29
+ * Time: 10:31
+ * Desc:
+ */
+//@Configuration
+@EnableCaching
+public class CachePConfig {
+
+}
