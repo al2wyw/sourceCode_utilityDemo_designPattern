1e0b54b2982e64755e1797853a4292b7cb1fa121
johnnyleeli <johnnyleeli@tencent.com>
2023-07-30 20:41:23 +0800

    add soot test

---
+++ spring_source_test/src/main/java/com/soot/Optimization.java
@@ -0,0 +1,43 @@
+package com.soot;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liyang
+ * Date: 2023-07-30
+ * Time: 20:12
+ * Description:
+ */
+public class Optimization {
+
+    public void copyPropagation() {
+        int a = 10;
+        int b = a + 1;
+        System.out.println(b);
+    }
+
+    public void copyPropagation2() {
+        int a = 10;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a);
+        }
+        int b = 11;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(b);
+        }
+    }
+
+    public void copyPropagation3() {
+        int a = 10;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a++);
+        }
+        int b = 11;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(b++);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
