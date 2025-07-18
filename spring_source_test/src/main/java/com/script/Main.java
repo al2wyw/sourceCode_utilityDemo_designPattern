1ccdf8dad1dc568fcb11ba9491ab149b1d51d55b
johnnyleeli <johnnyleeli@tencent.com>
2025-07-18 19:16:59 +0800

    add script test

---
+++ spring_source_test/src/main/java/com/script/Main.java
@@ -0,0 +1,75 @@
+package com.script;
+
+import com.github.pandora.antlr.AntlrCompiler;
+import com.github.pandora.ast.GenerateContext;
+import com.github.pandora.ast.ScriptNode;
+import com.github.pandora.compile.CompileContext;
+import com.github.pandora.eval.EvalTemplate;
+import com.github.pandora.eval.EvaluationContext;
+import com.github.pandora.eval.EvaluationValue;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello world!");
+        Integer i = Integer.valueOf("1233");
+        Float j = Float.valueOf("34.23");
+        Float k = 1 +
+                -j;
+        Double b = (double)k.floatValue();
+        if (j > i) {
+            System.out.println(k);
+        }
+        System.out
+                .println(k);
+
+
+        InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.tl");
+        Compiler compiler = new AntlrCompiler();
+        ScriptNode node = compiler.compile(stream, new CompileContext(Model.class));
+
+        GenerateContext ast = new GenerateContext(Model.class);
+        // GenerateContext.enableDebug();
+        // GenerateContext.enableOutPutClassFile(Thread.currentThread().getContextClassLoader().getResource("").getPath());
+        node.generateCode(ast);
+
+        EvalTemplate template = ast.getTemplateClass().newInstance();
+        Model model = new Model();
+        if (!model.flag1)
+            model.int3 = 100;
+        else if (model.int1 > 800)
+            model.int3 = model.int2 & 1;
+        else
+            model.int3 = 1;
+        EvaluationContext context = new EvaluationContext();
+        context.add("params", model);
+        EvaluationValue value = template.evaluate(context);
+        System.out.println(value.getValue());
+        System.out.println(model);
+    }
+
+    public Object eval(Object ob) {
+        Model model = (Model) ob;
+
+        model.int1 = 666;
+        model.int2 = 879;
+        model.float1 = 12.34f;
+        model.float2 = 13.44f;
+        model.name1 = "my peter";
+        model.flag1 = model.int1 >= model.int1;
+        model.flag2 = model.float1 > model.int2;
+        model.flag3 = model.flag1 && true;
+        model.double1 = (model.float1 + model.float2) * model.int1;
+        // model.ret = (model.float1 + model.float2) * model.int1;
+        model.int3 = model.int1 * (model.int1 + model.int2);
+
+        if (model.int1 < 100 || model.flag1)
+            model.int3 = 100;
+        else if (model.int1 > 800)
+            model.int3 = 10;
+        else
+            model.int3 = 1;
+
+        return null;
+    }
+}
---
+++ spring_source_test/src/main/java/com/script/Model.java
@@ -0,0 +1,46 @@
+package com.script;
+
+public class Model {
+
+    public int int1;
+    public int int2;
+    public int int3;
+    public String name1;
+    public String name2;
+    public String name3;
+    public boolean flag1;
+    public boolean flag2;
+    public boolean flag3;
+
+    public double double1;
+    public double double2;
+    public double double3;
+
+    public float float1;
+    public float float2;
+    public float float3;
+
+    public Double ret;
+
+    @Override
+    public String toString() {
+        return "Model{" +
+                "double1=" + double1 +
+                ", int1=" + int1 +
+                ", int2=" + int2 +
+                ", int3=" + int3 +
+                ", name1='" + name1 + '\'' +
+                ", name2='" + name2 + '\'' +
+                ", name3='" + name3 + '\'' +
+                ", flag1=" + flag1 +
+                ", flag2=" + flag2 +
+                ", flag3=" + flag3 +
+                ", double2=" + double2 +
+                ", double3=" + double3 +
+                ", float1=" + float1 +
+                ", float2=" + float2 +
+                ", float3=" + float3 +
+                ", ret=" + ret +
+                '}';
+    }
+}
